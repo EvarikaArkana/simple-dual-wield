fafe1b29cc526bda2a70863d04e5d483e83532e9
EvarikaArkana <evarikaarkana@proton.me>
2025-05-13 09:38:08 -0400

    initial commit

---
+++ src/client/java/eva/dualwield/SimpleDualWieldClient.java
@@ -0,0 +1,10 @@
+package eva.dualwield;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleDualWieldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
